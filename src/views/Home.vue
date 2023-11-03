1a007c646a108edf0b01207a6603b87c5c1cb8a1
Tom Österlund <tom_jonathan.oesterlund@prosiebensat1.com>
2023-11-03 22:07:14 +0100

    demo

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="calendar-container is-light-mode">
+    <Qalendar :events="events" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { Qalendar } from "qalendar";
+  import { ref } from "vue";
+
+  const events = ref([
+    {
+      id: 1,
+      title: 'hello',
+      time: {
+        start: '2023-11-03 10:00',
+        end: '2023-11-03 12:00'
+      }
+    }
+  ])
+</script>
+
+<style>
+@import "qalendar/dist/style.css";
+
+.calendar-container {
+  width: 100%;
+  height: 800px;
+  left: 0;
+}
+</style>
